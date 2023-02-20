37276dbb2b3df8142dd08f02df8ae05016eea590
Grzegorz Pasterny <grzegorz.pasterny@zf.com>
2023-02-20 07:22:27 +0100

    Add LoginDialog and authenticator excercise

---
+++ PPTA_Excercises/Excercise_2/Authenticator/IAuthenticator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPTA_Excercises.Excercise_2.Authenticator
+{
+    public interface IAuthenticator
+    {
+        public bool Authenticate(string userName, string password);
+    }
+}
---
+++ PPTA_Excercises/Excercise_2/Authenticator/LoginDialog.cs
@@ -0,0 +1,58 @@
+﻿namespace PPTA_Excercises.Excercise_2.Authenticator
+{
+    public class LoginDialog
+    {
+        private readonly IAuthenticator authenticator;
+
+        public LoginDialog(IAuthenticator authenticator)
+        {
+            this.authenticator = authenticator;
+        }
+
+        /// <summary>
+        /// Shows the control that enables to login by the user
+        /// </summary>
+        public void Show()
+        {
+            // request to the GUI that shows login dialog
+        }
+
+        /// <summary>
+        /// Button event while user submits <paramref name="userName"/> and <paramref name="password"/>
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public bool Submit(string userName, string password)
+        {
+            if (authenticator.Authenticate(userName, password))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Closes the dialog
+        /// </summary>
+        /// <returns></returns>
+        public bool Close()
+        {
+            // some GUI operations
+            return true;
+        }
+    }
+
+    // test cases:
+    // 1. Test if dialog closes successfully before user name and password are submitted. Use dummy authenticator
+    // 2. Test if Submit returns false when wrong user name and password are provided and true when credenitals are correct. Use dummy authenticator
+    // 3. Test if LoginDialog correctly invokes authenticator class:
+    // authenticator should only be invoked once and should have the user name and password passed to the Submit method. Use authenticator spy
+    // 4. Assume some expectedUserName and expectedPassword and test if LoginDialog correctly invokes Authenticator like in previous test
+    // and validate all values provided to authenticator. Use mock authenticator
+    // 5. Assume some arbitrary user name and password that are considered as correct credentials and check if LoginDialog's Submit method returns correct result
+    // after providing correct and incorrect credentials. Use fake authenticator
+}
