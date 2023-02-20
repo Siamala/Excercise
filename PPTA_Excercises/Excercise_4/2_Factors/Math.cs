9a93cb80360fc686285a46898b5cf91245c2bcf4
Grzegorz Pasterny <grzegorz.pasterny@zf.com>
2023-02-20 16:55:22 +0100

    Add Math FactorsOf method stub

---
+++ PPTA_Excercises/Excercise_4/2_Factors/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPTA_Excercises.Excercise_4._2_Factors
+{
+    internal class Math
+    {
+        static List<int> FactorsOf(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
