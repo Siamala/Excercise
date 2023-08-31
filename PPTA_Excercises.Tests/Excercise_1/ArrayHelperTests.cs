eafd6c4769e1a5d16254677c6922b8eb92d4a242
Siamala <86688267+Siamala@users.noreply.github.com>
2023-08-31 19:42:08 +0200

    commit

---
+++ PPTA_Excercises.Tests/Excercise_1/ArrayHelperTests.cs
@@ -0,0 +1,108 @@
+﻿using Excercise_1;
+using FluentAssertions;
+using System.Linq;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace PPTA_Excercises.Tests.Excercise_1
+{
+    public class ArrayHelperTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            string[] array = { "jabłko", "masło", "kurczak", "witamina" };
+
+            // Act
+            var result = ArrayHelper.RemoveEmptyElements(array);
+
+            // Assert
+            result.Length.Should().Be(4);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            // Arrange
+            string[] array = { };
+
+            // Act
+            var result = ArrayHelper.RemoveEmptyElements(array);
+
+            // Assert
+            result.Length.Should().Be(0);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            // Arrange
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+            int size = 3;
+
+            // Act
+            var result = arr.Split(size).ToList();
+
+            // Assert
+            Assert.Equal(3, result.Count);
+            Assert.Equal(new[] { 1, 2, 3 }, result[0]);
+            Assert.Equal(new[] { 4, 5, 6 }, result[1]);
+            Assert.Equal(new[] { 7 }, result[2]);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            // Arrange
+            int[] arr = new int[0];
+            int size = 2;
+
+            // Act
+            var result = arr.Split(size).ToList();
+
+            // Assert
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            // Arrange
+            int[] myArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            // Act
+            int[] slicedArray = myArray.Slice(2, 4); // Zwróci tablicę { 3, 4, 5, 6 }
+
+            // Assert
+            slicedArray.Should().BeEquivalentTo(new[] { 3, 4, 5, 6 });
+        }
+
+
+        [Fact]
+        public void Test6()
+        {
+            // Arrange
+            int[] source = { 1, 2, 3, 4, 5 };
+
+            // Act
+            int[] slice = source.Slice(2, 0);
+
+            // Assert
+            Assert.Empty(slice);
+        }
+
+        [Fact]
+        public void Test7()
+        {
+            // Arrange
+            int[] source = { 1, 2, 3, 4, 5 };
+
+            // Act
+            int[] slice = source.SliceWithPadding(2, 0);
+
+            // Assert
+            Assert.Empty(slice);
+        }
+    }
+}
---
+++ PPTA_Excercises.Tests/Excercise_1/StringHelperTests.cs
@@ -0,0 +1,103 @@
+﻿using Excercise_1;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace PPTA_Excercises.Tests.Excercise_1
+{
+    public class StringHelperTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            var fileName = "calc.exe";
+
+            // Act
+            var result = StringHelper.AddDirectoryToPathFile(fileName, string.Empty);
+
+            // Assert
+            result.Should().Be("calc.exe");
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            // Arrange
+            var name = string.Empty;
+
+            // Act
+            var result = StringHelper.AddDirectoryToPathFile(name, name);
+
+            // Assert
+            result.Length.Should().Be(0);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            // Arrange
+            string input = "abcdefghijklmnopqrstuvwxyz";
+            int limit = 5;
+
+            // Act
+            var chunks = input.SplitLimit(limit);
+
+            // Assert
+            string[] expectedChunks = { "abcde", "fghij", "klmno", "pqrst", "uvwxy", "z" };
+            Assert.Equal(expectedChunks, chunks);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            // Arrange
+            string fileName = "good_filename.txt";
+
+            // Act
+            string sanitizedFileName = fileName.RemoveInvalidFileNameCharacters();
+
+            // Assert
+            Assert.Equal(fileName, sanitizedFileName);
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            // Arrange
+            string input = "0A-1B-2C-GF";
+            char separator = '-';
+
+            // Act
+            bool result = input.TryParseHexStringWithSeparatorToByteArray(separator, out byte[] byteArray);
+
+            // Assert
+            Assert.False(result);
+            Assert.Null(byteArray);
+        }
+
+        [Fact]
+        public void Test6()
+        {
+            // Arrange
+            string hexString = "GHIJKL";
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => StringHelper.HexStringToByteArray(hexString));
+        }
+
+        [Fact]
+        public void Test7()
+        {
+            // Arrange
+            string hexString = "0x1A2B3C4D";
+            byte[] expectedBytes = new byte[] { 0x1A, 0x2B, 0x3C, 0x4D };
+
+            // Act
+            byte[] result = StringHelper.HexStringToByteArray(hexString);
+
+            // Assert
+            Assert.Equal(expectedBytes, result);
+        }
+    }
+}
